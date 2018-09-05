7614a408babc1d77776bd0f3adecf47cefc00874
sumeys <sumeys@163.com>
2018-09-05 16:55:57 +0800

    优化一下归并排序

--- src/com/sumey/sort/MergeSort.java
+++ src/com/sumey/sort/MergeSort.java
@@ -11,22 +11,22 @@ package com.sumey.sort;
  */
 public class MergeSort {
 
-    private static void mergeSort(int[] A, int n) {
-        mSort(A, 0, n - 1);
+    private static void mergeSort(int[] A, int n, int[] temp) {
+        mSort(A, 0, n - 1, temp);
     }
 
-    private static void mSort(int[] A, int left, int right) {
+    private static void mSort(int[] A, int left, int right, int[] temp) {
         if (left >= right) {
             return;
         }
         int mid = (left + right) / 2;
-        mSort(A, left, mid);
-        mSort(A, mid + 1, right);
-        merge(A, left, mid, right);
+        mSort(A, left, mid, temp);
+        mSort(A, mid + 1, right, temp);
+        merge(A, left, mid, right, temp);
     }
 
-    private static void merge(int[] A, int left, int mid, int right) {
-        int[] temp = new int[right - left + 1];
+    private static void merge(int[] A, int left, int mid, int right, int[] temp) {
+        //int[] temp = new int[right - left + 1];  节约每次都去申请数组
         int leftIndex = left;
         int rightIndex = mid + 1;
         int tempIndex = 0;
@@ -48,7 +48,8 @@ public class MergeSort {
 
     public static void main(String[] args) {
         int[] a = {3, 0, 9, 18, 1, 4, 6};
-        mergeSort(a, a.length);
+        int[] temp = new int[a.length];
+        mergeSort(a, a.length, temp);
         for (int x : a
                 ) {
             System.out.println(x);
